adcd094ab711e7201f0ace5948bdc742b3775dbd
kake <Makky.rrr.omach@yahoo.ne.jp>
2024-10-10 00:38:14 +0900

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default withNuxt(
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/no-multiple-template-root': 'error',
+      'vue/multi-word-component-names': 'error',
+      'vue/require-v-for-key': 'error',
+      'vue/no-use-v-if-with-v-for': 'error',
+    },
+  },
+  stylistic.configs.customize({
+    indent: 2,
+    trailingComma: 'es5',
+    tabWidth: 2,
+    useTabs: true,
+    singleQuote: true,
+    bracketSpacing: true,
+  }),
+)
